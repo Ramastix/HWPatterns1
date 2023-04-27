efbbd772694c74b743686291dd541c971ae8210f
Ramastix <ramastix@gmail.com>
2023-04-27 16:25:19 +0300

    allure

--- src/test/java/ru/netology/paterns/test/TestClass.java
+++ src/test/java/ru/netology/paterns/test/TestClass.java
@@ -2,6 +2,10 @@ package ru.netology.paterns.test;
 
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -14,7 +18,15 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 public class TestClass {
+    @BeforeAll
+    static void setUpAll(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
 
+    @AfterAll
+    static void tearDownAll(){
+        SelenideLogger.removeListener("allure");
+    }
     @BeforeEach
     void setup() {open("http://localhost:9999/");}
 
